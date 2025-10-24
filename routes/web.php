ec2234042f327aed8242494201416dde55c687fe
Rafi-Andi <rafiandiprayitno9528@gmail.com>
2025-10-24 12:57:16 +0700

    Migration add table

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\KelasController;
+use App\Http\Controllers\PelajaranController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', [HelloController::class, 'view']);
+Route::get('user', [UserController::class, 'index']);
+
+Route::get('/siswa', [SiswaController::class, 'index']);
+Route::get('/kelas', [KelasController::class, 'index']);
+Route::get('/pelajaran', [PelajaranController::class, 'index']);
